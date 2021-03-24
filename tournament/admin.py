abb74d7756b19cc6a2bfbd417008bfb263b3641c
Guillaume Esquevin <gesquevin@csod.com>
2021-03-24 11:46:14 +0100

    Expose all models in admin
    Even though teamplayercard and tounamentplayercard
    should not be tampered with manually.
    
    It's for demonstration purpose only and I would not make them accessible
    in a production web app.

--- tournament/admin.py
+++ tournament/admin.py
@@ -3,8 +3,12 @@ from .models import (
     Tournament,
     League,
     Team,
+    TeamPlayerCard,
+    TournamentPlayerCard,
 )
 
 admin.site.register(Tournament)
 admin.site.register(League)
 admin.site.register(Team)
+admin.site.register(TeamPlayerCard)
+admin.site.register(TournamentPlayerCard)
